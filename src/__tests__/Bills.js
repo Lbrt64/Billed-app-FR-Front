1c2ef568438888ee476f62051a469c920f0e9014
Lbrt64 <lbruyant@outlook.fr>
2022-10-31 18:44:05 +0100

    Add new NewBill tests

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -3,11 +3,8 @@
  */
 
 // import test tools
-import {screen, waitFor} from "@testing-library/dom"
+import { screen } from "@testing-library/dom"
 import userEvent from '@testing-library/user-event'
-
-// import views
-import BillsUI from "../views/BillsUI.js"
 
 // import mocked data
 import { bills } from "../fixtures/bills.js"
@@ -16,31 +13,29 @@ import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store"
 
 // import app logic
-import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
-import Bills from "../containers/Bills.js";
+import { ROUTES_PATH} from "../constants/routes.js";
 import router from "../app/Router.js";
 
 // setup 
 jest.mock("../app/store", () => mockStore)
-const onNavigate = (pathname) => {document.body.innerHTML = ROUTES({ pathname })}
 $.fn.modal = jest.fn();
 
 describe("Given I am connected as an employee", () => {
 
-  beforeEach(() => {
-    // Sets the localstorage information and identify user as employee
-    Object.defineProperty(window, localStorage, { value: localStorageMock });
-    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "a@a" }))
-    // Requirements to launch router
-    const root = document.createElement("div")
-    root.setAttribute("id", "root")
-    document.body.appendChild(root)
-    // Launch router and generate Bills page
-    router()
-    window.onNavigate(ROUTES_PATH.Bills)
-  })
-
   describe("When I am on Bills Page", () => {
+
+    beforeEach(() => {
+      // Sets the localstorage information and identify user as employee
+      Object.defineProperty(window, localStorage, { value: localStorageMock });
+      window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "a@a" }))
+      // Requirements to launch router
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      // Launch router and generate Bills page
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+    })
 
     test("Then bill icon in vertical layout should be highlighted", () => {
       const windowIcon = screen.getByTestId('icon-window')
@@ -51,68 +46,66 @@ describe("Given I am connected as an employee", () => {
       let orderedBills = bills.sort(function(a,b) {return new Date(a.date) - new Date(b.date)})
       expect(orderedBills).toEqual(chronoOrderedBills)
     })
-  })
 
-  describe("When I click on the eye icon for a bill", () => {
-    test('Then a modal should open', () => {
-      const eye = screen.getAllByTestId('icon-eye')[0]
-      userEvent.click(eye)
-      expect(screen.getByTestId("modaleFile")).toBeTruthy()
+    describe("When I click on the eye icon for a bill", () => {
+      test('Then a modal should open', () => {
+        const eye = screen.getAllByTestId('icon-eye')[0]
+        userEvent.click(eye)
+        expect(screen.getByTestId("modaleFile")).toBeTruthy()
+      })
+    })
+  
+    // [Ajout de tests unitaires et d'intégration] - Bills
+    describe("When I click on new bill icon", () => {
+      test('Then new bill form should be displayed', () => {
+        const newBillButton = screen.getByTestId('btn-new-bill')
+        userEvent.click(newBillButton)
+        expect(screen.getByTestId("form-new-bill")).toBeTruthy()
+      })
+    })
+  
+    // [Ajout de tests unitaires et d'intégration] - Bills GET API positive scenario
+    describe("When data received from GET Bills API is correct", () => {
+      test("Then bills data should be displayed on the Bills page", async () => {
+        expect(screen.getByTestId("tbody")).toBeTruthy()
+      })
+    })
+  
+    // [Ajout de tests unitaires et d'intégration] - Bills GET API error scenario
+    describe("When an error occurs on GET Bills API", () => {
+  
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+      })
+  
+      test("Then in case of API 404 error, the page should display it", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 404"))
+            }
+          }})
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+  
+      test("Then in case of API 500 error, the page should display it", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }})
+  
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+  
     })
   })
-
-  // [Ajout de tests unitaires et d'intégration] - Bills
-  describe("When I click on new bill icon", () => {
-    test('Then new bill form should be displayed', () => {
-      const newBillButton = screen.getByTestId('btn-new-bill')
-      userEvent.click(newBillButton)
-      expect(screen.getByTestId("form-new-bill")).toBeTruthy()
-    })
-  })
-
-  // [Ajout de tests unitaires et d'intégration] - Bills GET API positive scenario
-  describe("When I load the Bills Page and data received from GET Bills API is correct", () => {
-    test("Then bills data should be displayed on the Bills page", async () => {
-      expect(screen.getByTestId("tbody")).toBeTruthy()
-    })
-  })
-
-  // [Ajout de tests unitaires et d'intégration] - Bills GET API error scenario
-  describe("When I load the Bills Page and an error occurs on GET Bills API", () => {
-
-    beforeEach(() => {
-      jest.spyOn(mockStore, "bills")
-    })
-
-    test("Then in case of API 404 error, the page should display it", async () => {
-      mockStore.bills.mockImplementationOnce(() => {
-        return {
-          list : () =>  {
-            return Promise.reject(new Error("Erreur 404"))
-          }
-        }})
-      window.onNavigate(ROUTES_PATH.Bills)
-      await new Promise(process.nextTick);
-      const message = await screen.getByText(/Erreur 404/)
-      expect(message).toBeTruthy()
-    })
-
-    test("Then in case of API 500 error, the page should display it", async () => {
-      mockStore.bills.mockImplementationOnce(() => {
-        return {
-          list : () =>  {
-            return Promise.reject(new Error("Erreur 500"))
-          }
-        }})
-
-      window.onNavigate(ROUTES_PATH.Bills)
-      await new Promise(process.nextTick);
-      const message = await screen.getByText(/Erreur 500/)
-      expect(message).toBeTruthy()
-    })
-
-  })
-
-
 })
 
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,80 +2,83 @@
  * @jest-environment jsdom
  */
 
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-import {fireEvent, screen, waitFor} from "@testing-library/dom"
+// import test tools
+import { fireEvent, screen } from "@testing-library/dom"
 import userEvent from '@testing-library/user-event'
-import BillsUI from "../views/BillsUI.js"
+
+// import mocked data
 import { bills } from "../fixtures/bills.js"
 import { chronoOrderedBills } from "../fixtures/chronoOrderedBills.js"
-import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
-import Bills from "../containers/Bills.js";
-import {localStorageMock} from "../__mocks__/localStorage.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store"
+
+// import app logic
+import { ROUTES_PATH} from "../constants/routes.js";
+import NewBill from "../containers/NewBill.js";
 import router from "../app/Router.js";
-import isImage from "../app/utils.js"
+import { isImage } from "../app/utils.js";
 
+// setup 
 jest.mock("../app/store", () => mockStore)
-const store = mockStore
-const onNavigate = (pathname) => {
-    document.body.innerHTML = ROUTES({ pathname })
-}
-const testNewBill = new NewBill({document, onNavigate, store, localStorageMock})
-
+const onNavigate = (pathname) => {document.body.innerHTML = ROUTES({ pathname })}
 
 describe("Given I am connected as an employee", () => {
-    let testNewBill = ''
-    beforeEach(() => {
-     Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-     window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-    }))
-    const store = mockStore
-    const root = document.createElement("div")
-    root.setAttribute("id", "root")
-    document.body.append(root)
-    const onNavigate = (pathname) => {
-        document.body.innerHTML = ROUTES({ pathname })
-    }
-    testNewBill = new NewBill({document, onNavigate, store, localStorageMock})
-    router()
-    console.log(screen)
-    // window.onNavigate(ROUTES_PATH.NewBill)
-    })
 
-    describe("When I am on new bills form page and load a new file", () => {
+    describe("When I am on New Bills Page", () => {
+  
+      beforeEach(() => {
+        // Sets the localstorage information and identify user as employee
+        Object.defineProperty(window, localStorage, { value: localStorageMock });
+        window.localStorage.setItem('user', JSON.stringify({ type: 'Employee', email: "a@a" }))
+        // Requirements to launch router
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.appendChild(root)
+        // Launch router and generate Bills page
+        router()
+        window.onNavigate(ROUTES_PATH.NewBill)
+      })
+  
+      test("Then the page title should be Envoyer une note de frais", () => {
+        expect(screen.getByText(/Envoyer une note de frais/)).toBeTruthy()
+      }) 
 
-        test("Then if it's a png it appears in the form field", async () => {
-            const handleChangeFile = jest.fn(testNewBill.handleChangeFile)
-            const testFile = screen.getByTestId('file')
-            testFile.addEventListener('change', handleChangeFile)
-            fireEvent.change(inputFile, {
-              target: {
-                files: [new File(['file.png'], 'file.png')]
-              }
-            })
+      describe("When I upload a file", () => {
+        test("Then the handleChangeFile function should be called", () => {
+            const file = new File(['document'], 'document.png')
+            const input = screen.getByTestId('file')
+            const handleChangeFile = jest.fn()
+            input.addEventListener("change", handleChangeFile)
+            userEvent.upload(input, file)
             expect(handleChangeFile).toHaveBeenCalled()
+        })
+
+        test("Then if the format is .png it should be accepted", async () => {
+            const fileName = 'document.png'
+            expect(isImage(fileName)).toBeTruthy();
+        })
+
+        test("Then if the format is .pdf it should be refused", async () => {
+            const fileName = 'document.pdf'
+            expect(isImage(fileName)).toBeFalsy();
+        })
+
+        // test("Then if the format is .pdf it should not be displayed on the page", () => {
+        //     // définir valeur
+        //     // handle
+        // })
+        }) 
+
+      describe("When I submit the form", () => {
+        test("Then handleSubmit function should be called and loading screen is displayed", () => {
+            const formNewBill = screen.getByTestId('form-new-bill')
+            const handleSubmit = jest.fn()
+            const submitButton = screen.getByText('Envoyer', { exact: true })
+            formNewBill.addEventListener('submit', handleSubmit)
+            userEvent.click(submitButton)
+            expect(handleSubmit).toHaveBeenCalled()
             })
-
-//         test("Then if it is a pdf, the form field becomes empty", async () => {
-//             expect()
-//         })
-
-//     })
-
-//     describe("When I am on new bills form page and submit a bill", () => {
-        
-//         test("Then handleSubmit is called", async () => {
-//           expect()
-//         })
-
-//         test("Then I am taken to the Bills page", async () => {
-//           expect()
-//         })
-
+        })
     })
 })
-
-
-// POST TEST
+  
